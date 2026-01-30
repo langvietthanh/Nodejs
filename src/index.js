592bb34302e2f58616cb00b0c07f63f2653c5087
lang thanh <langvietthanh.28072005@gmail.com>
2026-01-30 21:49:37 +0700

    delete src/public/css and Form default behavior, Query parameters

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const port = 3000
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 // Template engine
 app.engine('hbs', handlebars({
@@ -16,12 +16,20 @@ app.engine('hbs', handlebars({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname,'resources/views'));
  
-
 app.get('/', (req, res) => {
   res.render('home');
 })
 
 app.get('/tin-tuc', (req, res) => {
+  res.render('news');
+})
+
+app.get('/search', (req,res) => {
+  res.render('search');
+})
+
+app.post('/tin-tuc', (req,res) => {
+  console.log(req.query, 'abc')
   res.render('news');
 })
 
